038ada1187b58f8ee6b2037076f2277a13a98a80
mgermaine93 <mgermaine93@gmail.com>
2020-05-14 21:15:47 -0400

    Added chalk and validate packages, plus modules.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,16 @@
-// Loads in the file named "utils.js" in the notes-app folder
+// Challenge:  Use the Chalk Library in your project
+// 1. Install version 2.4.1 of Chalk
+// 2. Load Chalk into app.js
+// 3. Use it to print the string "Success!" into the console in green text
+// 4. Test your work
+// Bonus:  Make text bold and inversed
+
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
 // Calls the function itself
 const message = getNotes();
-
 console.log(message);
+
+// The methods come from the https://www.npmjs.com/package/chalk documentation
+console.log(chalk.greenBright.bold.inverse("Success!"));

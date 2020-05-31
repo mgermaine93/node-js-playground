c555605015e24dc5528541e26498e0d9e0524400
mgermaine93 <mgermaine93@gmail.com>
2020-05-30 22:12:07 -0400

    Added saveNotes function and error handling.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,12 +1,42 @@
-// Challenge:  Define and use a function in a new file.
-//  1. Create a new file called notes.js.
-//  2. Create getNotes function returns "Your notes..."
-//  3. Export getNotes function.
-//  4. From app.js, load in and call the function printing message to console.
+const fs = require("fs");
 
 const getNotes = function () {
   return "Your notes...";
 };
 
+const addNote = function (title, body) {
+  const notes = loadNotes();
+
+  notes.push({
+    title: title,
+    body: body,
+  });
+
+  saveNotes(notes);
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  // Try doing this...
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+    // If the above doesn't work, do this
+  } catch (error) {
+    // If there is no file, return an empty array
+    return [];
+  }
+};
+
 // Exports the file.
-module.exports = getNotes;
+module.exports = {
+  // Value comes from the getNote function above
+  getNote: getNotes,
+  // Value comes from the addNote function above
+  addNote: addNote,
+};

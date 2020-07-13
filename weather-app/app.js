cd9dd9b9449102b010822b9072cc20931d219a93
mgermaine93 <mgermaine93@gmail.com>
2020-07-13 17:07:59 -0400

    Added Air Quality API Call.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,14 +2,18 @@ const request = require("request");
 
 // API key
 // const url =
-("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=NGSqAMa4p7cmXliXfrMMsNrFo8tNQCydFkl7SpHx");
+// ("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=NGSqAMa4p7cmXliXfrMMsNrFo8tNQCydFkl7SpHx");
 
-const url = "https://pokeapi.co/api/v2/pokemon";
+// const url = "https://pokeapi.co/api/v2/pokemon";
+
+const url =
+  "http://api.airvisual.com/v2/nearest_city?key=5b31a517-34b8-4acb-8b7c-8d40d24c526e";
 
 // "we would like to parse this request as json"
 request({ url: url, json: true }, (error, response) => {
   // Gets Bulbasaur
-  console.log(response.body.results[0].name);
+  // console.log(response.body.results[0].name);
+  console.log(response.body);
   // const data = JSON.parse(response.body);
   // console.log(data.results[0]);
   // console.log(

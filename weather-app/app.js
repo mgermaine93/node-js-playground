80af2914205ab872952cb76726d6908e4dc61985
mgermaine93 <mgermaine93@gmail.com>
2020-07-12 20:35:25 -0400

    Switched API to PokeAPI, now getting Pokemon names.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,18 @@
 const request = require("request");
 
 // API key
-const url =
-  "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=NGSqAMa4p7cmXliXfrMMsNrFo8tNQCydFkl7SpHx";
+// const url =
+("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=NGSqAMa4p7cmXliXfrMMsNrFo8tNQCydFkl7SpHx");
+
+const url = "https://pokeapi.co/api/v2/pokemon";
 
 // "we would like to parse this request as json"
 request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.results);
   // const data = JSON.parse(response.body);
   // console.log(data.results[0]);
-  console.log(
-    response.body.results[10].latest.aid.median_debt.completers.overall // what year does 10 represent?
-  );
+  // console.log(
+  // response.body.results[10].latest.aid.median_debt.completers.overall // what year does 10 represent?
 });
 
 // .debt.median_debt.completers.overall

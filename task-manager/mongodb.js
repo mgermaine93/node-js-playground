69a24077328b2a71a30542503bc31172918a74fa
mgermaine93 <mgermaine93@gmail.com>
2020-07-16 17:11:30 -0400

    Completed Inserting Documents challenge.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,7 +2,7 @@
 
 const mongodb = require("mongodb");
 
-// Create the mongo client, which gives us access to the function necessary to connect to the database and perform CRUD operations
+// This creates the mongo client, which gives us access to the function necessary to connect to the database and perform CRUD operations
 const MongoClient = mongodb.MongoClient;
 
 // This is the connection point (Protocol, IP Address, Port)
@@ -28,10 +28,65 @@ MongoClient.connect(
     // This is a reference to the database we want to manipulate
     const db = client.db(databaseName);
 
-    // This inserts a single document into the "books" collection
-    db.collection("books").insertOne({
-      title: "East of Eden",
-      author: "John Steinbeck",
-    });
+    // // This inserts a single document into the "books" collection
+    // // insertOne() is asynchronous
+    // db.collection("books").insertOne(
+    //   {
+    //     title: "East of Eden",
+    //     author: "John Steinbeck",
+    //     // "result" returns the data and the document id if things go well
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("An error occurred.  Unable to insert book.");
+    //     }
+    //     // "ops" is an array of documents, and this will print out all the documents inserted out to the console
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // // Add more than one document at a time
+    // db.collection("books").insertMany(
+    //   [
+    //     {
+    //       title: "Sing Unburied Sing",
+    //       author: "Jesmyn Ward",
+    //     },
+    //     {
+    //       title: "Broke Millennial",
+    //       author: "Erin Lowry",
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents!");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // Course challenge
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Practice piano.",
+          completed: true,
+        },
+        {
+          description: "Email website folks.",
+          completed: false,
+        },
+        {
+          description: "Run",
+          completed: true,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert tasks!");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );

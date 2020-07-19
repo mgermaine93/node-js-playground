ad4c5b824007dcbf7c32038129b6b8880dd2fbcf
mgermaine93 <mgermaine93@gmail.com>
2020-07-19 14:08:20 -0400

    Completed Querying Documents Challenges.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,15 +9,9 @@ const connectionURL = "mongodb://127.0.0.1:27017";
 // This is the database we'll connect to
 const databaseName = "task-manager";
 
-// Generates a new ID
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString());
-
 // This connects to the server and parses the URLs correctly
 MongoClient.connect(
   connectionURL,
-
   // { useNewUrlParser: true }, // deprecated
   { useUnifiedTopology: true },
   (error, client) => {
@@ -27,70 +21,29 @@ MongoClient.connect(
         "An error occurred.  Unable to connect to the database"
       );
     }
-
     // This is a reference to the database we want to manipulate
     const db = client.db(databaseName);
 
-    // // This inserts a single document into the "books" collection
-    // // insertOne() is asynchronous
-    // db.collection("books").insertOne(
-    //   {
-    //     title: "The Shipping News",
-    //     author: "E. Annie Proulx",
-    //     // "result" returns the data and the document id if things go well
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("An error occurred.  Unable to insert book.");
-    //     }
-    //     // "ops" is an array of documents, and this will print out all the documents inserted out to the console
-    //     console.log(result.ops);
-    //   }
-    // );
+    db.collection("tasks").findOne(
+      // Gets the last task in the tasks database
+      { _id: new ObjectID("5f10c03709aa9728dd9a1dd0") },
 
-    // // Add more than one document at a time
-    // db.collection("books").insertMany(
-    //   [
-    //     {
-    //       title: "Sing Unburied Sing",
-    //       author: "Jesmyn Ward",
-    //     },
-    //     {
-    //       title: "Broke Millennial",
-    //       author: "Erin Lowry",
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents!");
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
+      (error, task) => {
+        if (error) {
+          return console.log("Unable to fetch task");
+        }
+        console.log(task);
+      }
+    );
 
-    // // Course challenge
-    // db.collection("tasks").insertMany(
-    //   [
-    //     {
-    //       description: "Practice piano.",
-    //       completed: true,
-    //     },
-    //     {
-    //       description: "Email website folks.",
-    //       completed: false,
-    //     },
-    //     {
-    //       description: "Run",
-    //       completed: true,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert tasks!");
-    //     }
-    //     // Prints the operation output
-    //     console.log(result.ops);
-    //   }
-    // );
+    // Gets all tasks that haven't been completed
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return console.log("There are no tasks that haven't been completed.");
+        }
+        console.log(tasks);
+      });
   }
 );

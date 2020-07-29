0e5bae917cf933fd9ec24aac5860389343bc2194
mgermaine93 <mgermaine93@gmail.com>
2020-07-29 17:49:41 -0400

    Playing around with data validation and sanitization.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,6 @@
-// Imports Mongoose
+// Imports Mongoose and Validator
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 // Connects to the database
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
@@ -9,29 +10,77 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 });
 
 // Defines the model.  First argument is the name of the model, second argument is the fields you want
-const Task = mongoose.model("task", {
-  description: {
+const Book = mongoose.model("book", {
+  title: {
     type: String,
+    required: true,
+    trim: true,
   },
-  completed: {
-    type: Boolean,
+  authorEmail: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  author: {
+    type: String,
+    default: "Unknown",
+    validate(value) {
+      if (value != "Markus Zusak") {
+        throw new Error("Author must be Markus Zusak!");
+      }
+    },
   },
 });
 
 // Actually creates a new task in the database
-const task = new Task({
-  description: "Cook delicious chili dinner.",
-  completed: false,
+const book = new Book({
+  title: "    I Am The Messenger   ",
+  author: "Markus Zusak",
+  authorEmail: "zusak@books.com",
 });
 
 // Error handling
-task
+book
   .save()
   // When things go well
   .then(() => {
-    console.log(task);
+    console.log(book);
   })
   // When things don't go well
   .catch((error) => {
     console.log("Error!", error);
   });
+
+// // Defines the model.  First argument is the name of the model, second argument is the fields you want
+// const Task = mongoose.model("task", {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// // Actually creates a new task in the database
+// const task = new Task({
+//   description: "Cook delicious chili dinner.",
+//   completed: false,
+// });
+
+// // Error handling
+// task
+//   .save()
+//   // When things go well
+//   .then(() => {
+//     console.log(task);
+//   })
+//   // When things don't go well
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });

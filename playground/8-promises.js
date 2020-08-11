711539aed5411f95b2f7815f17ea8c692d44142f
mgermaine93 <mgermaine93@gmail.com>
2020-08-11 17:26:08 -0400

    Practicing with promise chaining.

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,15 +1,42 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve([1, 2, 3, 4, 5, 6]);
-    reject("Things went wrong!");
-  }, 2000);
-});
+// Accepts two numbers, a and b
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
-// This function only gets executed when things go well
-doWorkPromise
-  .then((result) => {
-    console.log("Success!", result);
+//// WITHOUT PROMISE CHAINING ////
+
+// add(1, 2)
+//   .then((sum) => {
+//     console.log(sum);
+//     add(sum, 5)
+//       .then((sum2) => {
+//         console.log(sum2);
+//       })
+//       .catch((error2) => {
+//         console.log(error2);
+//       });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//// WITH PROMISE CHAINING ////
+
+add(1, 1)
+  // This runs when the add(1, 1) promise is fulfilled
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
   })
+  // This run when the add(sum, 4) promise is fulfilled
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  // This occurs when neither of the above are fulfilled
   .catch((error) => {
-    console.log("Error!", error);
+    console.log(error);
   });

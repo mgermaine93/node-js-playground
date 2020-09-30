0c7ef9d5a6eda2ee39fa22edc10754c6dc67b760
mgermaine93 <mgermaine93@gmail.com>
2020-09-30 15:58:40 -0400

    Added task data tests.

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -2,9 +2,9 @@
 const jwt = require("jsonwebtoken"); // Generates the JSON web token
 const mongoose = require("mongoose"); // Generates an object id
 const User = require("../../src/models/user");
+const Task = require("../../src/models/task");
 
 const userOneId = new mongoose.Types.ObjectId();
-
 const userOne = {
   _id: userOneId,
   name: "Charles Campion",
@@ -17,14 +17,58 @@ const userOne = {
   ],
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "Frannie Goldsmith",
+  email: "frannie.goldsmith@thestand.com",
+  password: "somethingElseNew123!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET), // creates the JSON web token
+    },
+  ],
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "First task",
+  completed: false,
+  owner: userOneId,
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Second task",
+  completed: true,
+  owner: userOneId,
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Third task",
+  completed: true,
+  owner: userTwoId,
+};
+
 // This will assure that users are deleted before Jest actually runs its tests
 const setupDatabase = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
   await new User(userOne).save();
+  await new User(userTwo).save();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
   userOneId,
   userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setupDatabase,
 };

0f20f7359d9e29665b918194cba9110207830482
mgermaine93 <mgermaine93@gmail.com>
2020-11-23 17:36:07 -0500

    Cleaned up weather code and added .gitignore.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,18 @@
 const request = require("request");
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&units=imperial&APPID=${apiKey}`;
 
-// API key
-// const url =
-// ("https://api.data.gov/ed/collegescorecard/v1/schools?api_key=NGSqAMa4p7cmXliXfrMMsNrFo8tNQCydFkl7SpHx");
-
-// const url = "https://pokeapi.co/api/v2/pokemon";
-
-const url =
-  "http://api.airvisual.com/v2/nearest_city?key=5b31a517-34b8-4acb-8b7c-8d40d24c526e";
-
-// "we would like to parse this request as json"
+// First argument is the url, second argument is the function to run
+// The request is parsed as JSON
 request({ url: url, json: true }, (error, response) => {
-  // Gets Bulbasaur
-  // console.log(response.body.results[0].name);
-  console.log(response.body);
-  // const data = JSON.parse(response.body);
-  // console.log(data.results[0]);
-  // console.log(
-  // response.body.results[10].latest.aid.median_debt.completers.overall // what year does 10 represent?
+  console.log(
+    response.body.weather[0].main +
+      ". " +
+      " It is currently " +
+      response.body.main.temp +
+      " degrees F in Pittsburgh.  It feels like " +
+      response.body.main.feels_like +
+      " degrees F."
+  );
 });
-
-// .debt.median_debt.completers.overall

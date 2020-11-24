7cab2a44a1c092d1f0ba539d29e9d3ac9be8e1ab
mgermaine93 <mgermaine93@gmail.com>
2020-11-24 17:27:47 -0500

    Added error handling to both requests.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require("request");
-require("dotenv").config();
+require("dotenv").config(); // Adds .dotenv references to the API keys.
 const weatherApiKey = process.env.WEATHER_API_KEY;
 const mapboxApiKey = process.env.MAPBOX_API_KEY;
 const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&units=imperial&APPID=${weatherApiKey}`;
@@ -8,23 +8,31 @@ const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angel
 // First argument is the url, second argument is the function to run
 // The request is parsed as JSON
 request({ url: weatherUrl, json: true }, (error, response) => {
-  console.log(
-    response.body.weather[0].main +
-      ". " +
-      " It is currently " +
-      response.body.main.temp +
-      " degrees F in Pittsburgh.  It feels like " +
-      response.body.main.feels_like +
-      " degrees F."
-  );
+  if (error) {
+    console.log("Unable to connect to weather service.  Try again?");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location.  Try another search?");
+  } else {
+    const weatherOutlook = response.body.weather[0].main;
+    const mainTemp = response.body.main.temp;
+    const feelsLikeTemp = response.body.main.feels_like;
+    console.log(
+      `${weatherOutlook}.  It is currently ${mainTemp} degrees in Pittsburgh.  It feels like ${feelsLikeTemp}.`
+    );
+  }
 });
 
 // HTTP Request Challenge
 request({ url: mapboxUrl, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-
-  console.log(
-    `The latitude and longitude of Los Angeles, CA, is ${latitude} and ${longitude}, respectively.`
-  );
+  if (error) {
+    console.log("Unable to connect to weather service.  Try again?");
+  } else if (response.body.error) {
+    console.log("Unable to find location.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(
+      `The latitude and longitude of Los Angeles, CA, is ${latitude} and ${longitude}, respectively.`
+    );
+  }
 });

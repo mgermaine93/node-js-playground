e2bed9c3a3dda1aa33ca58cc66446ae1e1343d8e
mgermaine93 <mgermaine93@gmail.com>
2020-12-01 16:54:21 -0500

    Completed callback chaining challenge.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,29 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// Two arguments are typically passed in to callbacks: "error" and "data"/"response"
-// To change what city you're searching for, pass it in here
-geocode("Berkley", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+// Accesses the location command-line argument that the user provides
+const address = process.argv[2];
 
-// latitude, longitude, function
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+if (!address) {
+  console.log("Please provide an address.");
+} else {
+  // Two arguments are typically passed in to callbacks: "error" and "data"/"response"
+  // To change what city you're searching for, pass it in here
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    // latitude, longitude, function
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      // Callback chaining in action
+      if (error) {
+        return console.log(error);
+      }
+      // This is what runs if BOTH requests work successfully
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
+
+//-75.7088, 44.1545

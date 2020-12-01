64bacdf26dd8f6e805049603c1847a2ed7734b44
mgermaine93 <mgermaine93@gmail.com>
2020-12-01 17:24:51 -0500

    Added ES6 shorthand and destructuring notes.

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,35 @@
+// Object property shorthand
+
+const city = "Cheyenne";
+const state = "Wyoming";
+
+const location = {
+  city,
+  state,
+  country: "United States",
+};
+
+console.log(location);
+
+// Object destructuring
+
+const book = {
+  title: "Say Nothing",
+  author: "Patrick Radden Keefe",
+  publicationYear: "2019",
+  price: 25,
+};
+
+// const title = book.title;
+// const author = book.author;
+
+// const { title: bookTitle, author, salePrice = "20" } = book;
+// console.log(bookTitle);
+// console.log(author);
+// console.log(salePrice);
+
+const transaction = (type, { title, author }) => {
+  console.log(type, title, author);
+};
+
+transaction("order", book);

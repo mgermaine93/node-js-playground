071e5845d2a35bd11d1baeb657754ce5b89e79ed
mgermaine93 <mgermaine93@gmail.com>
2020-12-03 17:25:51 -0500

    Completed serving-up-static-assets challenge.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,8 +1,15 @@
+// Load in core modules
+const path = require("path");
+
 // "express" is a function, and we call it to create a new express application
 const express = require("express");
 
 // this actually creates the express application
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// This serves up the contents of the public directory
+app.use(express.static(publicDirectoryPath));
 
 // this tells the server what to send back
 // first argument is the route, second argument is a function that describes what we want to do when someone visits the particular route
@@ -10,16 +17,6 @@ const app = express();
 // Home page
 app.get("", (request, response) => {
   response.send("<h1>This is the Home Page.</h1>");
-});
-
-// Help page
-app.get("/help", (request, response) => {
-  response.send("This is the Help Page.");
-});
-
-// About page
-app.get("/about", (request, response) => {
-  response.send("<h1>This is the About Page</h1>");
 });
 
 // Weather page

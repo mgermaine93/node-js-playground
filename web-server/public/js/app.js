18fca685194d036f76314b2cb56b9c536b6462ea
mgermaine93 <mgermaine93@gmail.com>
2020-12-07 16:45:07 -0500

    Added Fetch Browser HTTP Request.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,15 @@
+// This client-side file fetches the forecast information
 console.log("This is the client-side JavaScript file.");
+
+// "fetch data from this url and then run this function"
+fetch("http://localhost:3000/weather?address=pittsburgh").then((response) => {
+  // this function runs when the JSON data has arrived and been parsed
+  response.json().then((data) => {
+    console.log(data);
+    if (data.error) {
+      console.log(data.error);
+    }
+    console.log(data.forecast);
+    console.log(data.location);
+  });
+});
